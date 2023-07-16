0f72598ff43fdb1739de07404de9965397ce6777
kylekim2123 <kyle2123@naver.com>
2023-07-16 19:29:49 +0900

    build: 프로젝트 초기 환경 빌드

---
+++ src/main/java/com/programmers/musicapp/MusicCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.programmers.musicapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicCommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/programmers/musicapp/MusicCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programmers.musicapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicCommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
